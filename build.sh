6d18cc7854a966c51c1761448f26f6b83649a3ff
andersaskeland <anders.askeland@gmail.com>
2021-04-20 15:16:14 +0200

    Moved build file

---
+++ build.sh
@@ -0,0 +1,9 @@
+cd "/Users/andersaskeland/GitHub/FreezeDB"
+source ~/.pyenv/versions/FreezeDB/bin/activate 
+
+pyside2-uic resources/user_interface/mainwindow.ui -o resources/user_interface/mainwindow.py
+pyside2-uic resources/user_interface/dialog_create_db.ui -o resources/user_interface/dialog_create_db.py
+
+pyside2-rcc resources/graphics/icons.qrc -o icons_rc.py
+
+python main.py
